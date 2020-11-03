287503c5d20b7041d4a8aceffe4142c3ee226728
hiroyaonoe <onoehiroya@gmail.com>
2020-11-03 16:23:16 +0000

    routerの設定

--- infrastructure/Routing.go
+++ infrastructure/Routing.go
@@ -23,8 +23,35 @@ func NewRouting(db *DB) *Routing {
 }
 
 func (r *Routing) setRouting() {
+    taskController := controllers.NewTaskController(r.DB)
     userController := controllers.NewUserController(r.DB)
-    r.Gin.GET("/user/:id", func (c *gin.Context) { userController.Get(c) })
+    authController := controllers.NewAuthController(r.DB)
+
+    engine := r.Gin
+    
+    // middleware
+
+
+    
+    engine.GET("/login", func (c *gin.Context) { authController.Login(c) })
+
+    v1 := engine.Group("/api/v1")
+    v1.POST("/tasks", func (c *gin.Context) { taskController.Create(c) })
+    v1.GET("/tasks/:id", func (c *gin.Context) { taskController.GetbyID(c) })
+    v1.PUT("/tasks/:id", func (c *gin.Context) { taskController.Update(c) })
+    v1.Delete("/tasks/:id", func (c *gin.Context) { taskController.Delete(c) })
+    v1.PUT("/tasks/:id/completed", func (c *gin.Context) { taskController.Switch(c) })
+    v1.GET("/tasks/date/:date", func (c *gin.Context) { taskController.GetbyDate(c) })
+    v1.GET("/tasks/date/from/:start/to/:end", func (c *gin.Context) { taskController.GetbyPeriod(c) })
+
+    v1.GET("/user", func (c *gin.Context) { userController.Get(c) })
+    v1.POST("/user", func (c *gin.Context) { userController.Create(c) })
+    v1.PUT("/user", func (c *gin.Context) { userController.Update(c) })
+    v1.DELETE("/user", func (c *gin.Context) { userController.Delete(c) })
+
+
+
+
 }
 
 func (r *Routing) Run() {
