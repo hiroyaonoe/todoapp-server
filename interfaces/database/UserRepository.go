73586865d589f19923be4bb7e59ec154465e15e8
hiroyaonoe <onoehiroya@gmail.com>
2020-11-03 14:04:49 +0000

    UserRepositoryとdatabaseの接続

--- interfaces/database/UserRepository.go
+++ interfaces/database/UserRepository.go
@@ -1,18 +1,30 @@
 package database
 
 import (
-    "errors"
     "github.com/jinzhu/gorm"
     "github.com/hiroyaonoe/todoapp-server/domain"
 )
 
+// UserRepository の具体的な実装
 type UserRepository struct {}
 
 func (repo *UserRepository) FindByID(db *gorm.DB, id int) (user domain.User, err error) {
     user = domain.User{}
-    db.First(&user, id)
-    if user.ID <= 0 {
-        return domain.User{}, errors.New("user is not found")
-    }
-    return user, nil
+    err = db.First(&user, id).Error
+    return
+}
+
+func (repo *UserRepository) Create(db *gorm.DB, u domain.User) (user domain.User, err error) {
+    err = db.Create(&u).Error
+    return u, err
+}
+
+func (repo *UserRepository) Update(db *gorm.DB, u domain.User) (user domain.User, err error) {
+    err = db.Save(&u).Error
+    return u, err
+}
+
+func (repo *UserRepository) Delete(db *gorm.DB, id int) (uid int, err error) {
+    err = db.Delete(&domain.User{}, id).Error
+    return id, err
 }
