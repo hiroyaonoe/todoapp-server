0bee6d7be81720185bafa48b7b9b5438760e059e
hiroyaonoe <onoehiroya@gmail.com>
2020-11-11 21:12:14 +0000

    user.createのtestを実装

--- interfaces/controllers/user_test.go
+++ interfaces/controllers/user_test.go
@@ -1,22 +1,23 @@
 package controllers
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/golang/mock/gomock"
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
-
-	"encoding/json"
 	"time"
 
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
 	"github.com/hiroyaonoe/todoapp-server/domain/entity"
 	"github.com/hiroyaonoe/todoapp-server/domain/mock_repository"
 	"github.com/hiroyaonoe/todoapp-server/usecase"
-	"reflect"
+	"github.com/jinzhu/gorm"
 )
 
-func TestUserControllerGet(t *testing.T) {
+func TestUserController_Get(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
@@ -24,7 +25,6 @@ func TestUserControllerGet(t *testing.T) {
 		userid              string
 		prepareMockDBRepo   func(db *mock_repository.MockDBRepository)
 		prepareMockUserRepo func(user *mock_repository.MockUserRepository)
-		// prepareMockContext func(context *MockContext)
 		wantMessage string
 		wantData    entity.User
 		wantErr     bool
@@ -108,6 +108,108 @@ func TestUserControllerGet(t *testing.T) {
 				Data    entity.User
 			}{}
 			err := json.Unmarshal(w.Body.Bytes(), &actualH)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if actualH.Message != tt.wantMessage {
+				t.Errorf("Get() message = %s, want = %s", actualH.Message, tt.wantMessage)
+			}
+			if !reflect.DeepEqual(actualH.Data, tt.wantData) {
+				t.Errorf("Get() user = %+v, want = %+v", actualH.Data, tt.wantData)
+			}
+
+		})
+	}
+}
+
+func TestUserController_Create(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name                string
+		createUser          entity.User
+		prepareMockDBRepo   func(db *mock_repository.MockDBRepository)
+		prepareMockUserRepo func(user *mock_repository.MockUserRepository)
+		wantMessage string
+		wantData    entity.User
+		wantErr     bool
+		wantCode    int
+	}{
+		{
+			name:   "正しくユーザを作成できる",
+			createUser: entity.User{
+				Name:      "username",
+				Password:  "password",
+				Email:     "example@example.com",
+			},
+			prepareMockDBRepo: func(db *mock_repository.MockDBRepository) {
+				db.EXPECT().Connect()
+			},
+			prepareMockUserRepo: func(user *mock_repository.MockUserRepository) {
+				user.EXPECT().Create(gomock.Any(), gomock.Any()).
+				DoAndReturn(func(db *gorm.DB, user *entity.User) error {
+					user.ID = 3
+					user.CreatedAt = time.Unix(100, 0)
+					user.UpdatedAt = time.Unix(100, 0)
+					// user = &entity.User{
+					// 	ID:        3,
+					// 	Name:      "username",
+					// 	Password:  "password",
+					// 	Email:     "example@example.com",
+					// 	CreatedAt: time.Unix(100, 0),
+					// 	UpdatedAt: time.Unix(100, 0),
+					// }
+					return nil
+				})
+			},
+			wantMessage: "success",
+			wantData: entity.User{
+				ID:    3,
+				Name:  "username",
+				Email: "example@example.com",
+			},
+			wantErr:  false,
+			wantCode: http.StatusOK,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gin.SetMode("test")
+			w := httptest.NewRecorder()
+			context, _ := gin.CreateTestContext(w)
+			body, err := json.Marshal(&tt.createUser)
+			if err != nil {
+				t.Fatal(err)
+			}
+			context.Request, _ = http.NewRequest("POST", "/user", bytes.NewBuffer(body))
+
+			// モックの準備
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			dbRepo := mock_repository.NewMockDBRepository(ctrl)
+			tt.prepareMockDBRepo(dbRepo)
+			userRepo := mock_repository.NewMockUserRepository(ctrl)
+			tt.prepareMockUserRepo(userRepo)
+
+			userController := &UserController{
+				Interactor: usecase.UserInteractor{
+					DB:   dbRepo,
+					User: userRepo,
+				},
+			}
+
+			userController.Create(context)
+
+			if w.Code != tt.wantCode {
+				t.Errorf("Get() code = %d, want = %d", w.Code, tt.wantCode)
+			}
+
+			actualH := struct {
+				Message string
+				Data    entity.User
+			}{}
+			err = json.Unmarshal(w.Body.Bytes(), &actualH)
 			if err != nil {
 				t.Fatal(err)
 			}
