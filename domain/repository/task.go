b4c354511c62ac8c6f8ab2fb5561889f52d437da
hiroyaonoe <onoehiroya@gmail.com>
2020-12-06 07:25:57 +0000

    taseRepoの修正

--- domain/repository/task.go
+++ domain/repository/task.go
@@ -10,9 +10,9 @@ import (
 
 // TaskRepository is interface of Task
 type TaskRepository interface {
-	FindByUser(db *gorm.DB, uid int) (tasks []entity.Task, err error)
-	FindByID(db *gorm.DB, id int) (task entity.Task, err error)
-	Create(db *gorm.DB, t entity.Task) (task entity.Task, err error)
-	Update(db *gorm.DB, t entity.Task) (task entity.Task, err error)
-	Delete(db *gorm.DB, id int) (taskid int, err error)
+	Create(db *gorm.DB, t *entity.Task) (err error)
+	// 	FindByUser(db *gorm.DB, uid int) (tasks []entity.Task, err error)
+	// 	FindByID(db *gorm.DB, id int) (task entity.Task, err error)
+	// 	Update(db *gorm.DB, t entity.Task) (task entity.Task, err error)
+	// 	Delete(db *gorm.DB, id int) (taskid int, err error)
 }
