b3cc37b651ccb48b2cabb39a1470e74181e0aac6
hiroyaonoe <onoehiroya@gmail.com>
2020-12-08 00:14:24 +0000

    usecase-taskの修正

--- usecase/task.go
+++ usecase/task.go
@@ -10,15 +10,15 @@ type TaskInteractor struct {
 	Task repository.TaskRepository
 }
 
-func (interactor *TaskInteractor) Create(task *entity.Task) (jsonTask *entity.TaskForJSON, err error) {
+func (interactor *TaskInteractor) Create(task *entity.Task) (err error) {
 	// databaseのnot null制約があるので不要？
 	// 不正なユーザーリクエストの判別(フィールドのうち少なくともひとつがnilの場合)
 	if task.Title.IsNull() || task.UserID.IsNull() || task.Date.IsNull() {
-		return nil, entity.ErrInvalidTask
+		return entity.ErrInvalidTask
 	}
 	// 不正なユーザーリクエストの判別(TaskIDがnilでない場合)
 	if !task.ID.IsNull() {
-		return nil, entity.ErrInvalidTask
+		return entity.ErrInvalidTask
 	}
 
 	// UUIDを付与
@@ -27,9 +27,6 @@ func (interactor *TaskInteractor) Create(task *entity.Task) (jsonTask *entity.Ta
 	db := interactor.DB.Connect()
 	// 新規Userを作成
 	err = interactor.Task.Create(db, task)
-	if err != nil {
-		return nil, err
-	}
-	jsonTask = task.ToTaskForJSON()
-	return jsonTask, nil
+
+	return
 }
